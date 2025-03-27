bf2d71d48294dce928cf585c4e13b51b79db854a
lyl <liyinlong>
2025-03-27 18:34:28 +0800

    feat-支持过滤ns

---
+++ util/middlewareinfo.go
@@ -0,0 +1,12 @@
+package util
+
+import (
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+)
+
+type RoleStatusInfo struct {
+	PodName        string      `json:"podName,omitempty"`
+	Role           string      `json:"role,omitempty"`
+	LastUpdateTime metav1.Time `json:"lastUpdateTime,omitempty"`
+	PodStatus      string      `json:"podStatus,omitempty"`
+}
---
+++ util/operatorutil.go
@@ -0,0 +1,60 @@
+package util
+
+import (
+	"sigs.k8s.io/controller-runtime/pkg/event"
+	"sigs.k8s.io/controller-runtime/pkg/predicate"
+	"strings"
+)
+
+func getNamespaceFilter() predicate.Funcs {
+	// 定义 Predicate，过滤命名空间
+	watchNamespace := GetParamFromEnv("WATCH_NAMESPACE")
+	nsMap := StringToMap(watchNamespace)
+	namespacePredicate := predicate.Funcs{
+		CreateFunc: func(e event.CreateEvent) bool {
+			return checkNamespaceExists(nsMap, e.Object.GetNamespace())
+		},
+		UpdateFunc: func(e event.UpdateEvent) bool {
+			return checkNamespaceExists(nsMap, e.ObjectOld.GetNamespace())
+		},
+		DeleteFunc: func(e event.DeleteEvent) bool {
+			return checkNamespaceExists(nsMap, e.Object.GetNamespace())
+		},
+		GenericFunc: func(e event.GenericEvent) bool {
+			return checkNamespaceExists(nsMap, e.Object.GetNamespace())
+		},
+	}
+	return namespacePredicate
+}
+
+func checkNamespaceExists(nsMap map[string]struct{}, ns string) bool {
+	if len(nsMap) == 0 {
+		return true
+	}
+	_, exists := nsMap[ns]
+	return exists
+}
+
+func StringToMap(input string) map[string]struct{} {
+	result := make(map[string]struct{})
+
+	// 如果输入为空，返回空map
+	if input == "" {
+		return result
+	}
+
+	// 按逗号分割字符串
+	pairs := strings.Split(input, ",")
+
+	for _, pair := range pairs {
+		// 去除空格
+		pair = strings.TrimSpace(pair)
+		if pair == "" {
+			continue
+		}
+
+		result[pair] = struct{}{}
+	}
+
+	return result
+}
