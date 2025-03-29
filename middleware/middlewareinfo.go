1d83a6630ea1b6159d45cd271fde60c11a2c0e56
lyl <liyinlong>
2025-03-29 22:43:02 +0800

    feat-抽象一些公共变量

--- middleware/middlewareinfo.go
+++ middleware/middlewareinfo.go
@@ -4,6 +4,12 @@ import (
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
+type BasicStatus struct {
+	Status     string           `json:"status,omitempty"`
+	Phase      string           `json:"phase,omitempty"`
+	RoleStatus []RoleStatusInfo `json:"roleStatus,omitempty"`
+}
+
 type RoleStatusInfo struct {
 	PodName        string      `json:"podName,omitempty"`
 	Role           string      `json:"role,omitempty"`
