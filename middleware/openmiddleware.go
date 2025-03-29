7f80b001b4b91ad001365684ae9ad4beb6d12c17
lyl <liyinlong>
2025-03-29 21:59:36 +0800

    feat-抽象一些中间件公共方法

--- middleware/openmiddleware.go
+++ middleware/openmiddleware.go
@@ -3,10 +3,10 @@ package middleware
 import "context"
 
 type OpenMiddleware interface {
+	PrepareInitConfigMap(ctx context.Context, middlewareName, middlewareNamespace string) error
+	PrepareMiddlewareConfigMap(ctx context.Context, middlewareName, middlewareNamespace string) error
 	PrepareService(ctx context.Context, middlewareName, middlewareNamespace string) error
 	PrepareHeadlessService(ctx context.Context, middlewareName, middlewareNamespace string) error
 	PreparePodService(ctx context.Context, middlewareName, middlewareNamespace string) error
-	PrepareInitConfigMap(ctx context.Context, middlewareName, middlewareNamespace string) error
-	PrepareMiddlewareConfigMap(ctx context.Context, middlewareName, middlewareNamespace string) error
 	UpdateMiddlewareStatus(ctx context.Context, middlewareName, middlewareNamespace string) error
 }
