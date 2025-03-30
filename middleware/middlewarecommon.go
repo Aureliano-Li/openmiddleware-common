46c67511315d83b5cb56ea6a2af1ddb7ae0727df
lyl <liyinlong>
2025-03-30 08:26:55 +0800

    feat-抽象一些公共变量

--- middleware/middlewarecommon.go
+++ middleware/middlewarecommon.go
@@ -1,8 +1,19 @@
 package middleware
 
 import (
+	"context"
+	"fmt"
+	"github.com/Aureliano-Li/openmiddleware-common/util"
+	"io/ioutil"
 	corev1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/types"
+	"sigs.k8s.io/controller-runtime/pkg/client"
 	"strconv"
+)
+
+var (
+	mlog = util.GetLogger()
 )
 
 // GetHeadlessServiceName 获取中间件headless svc name
@@ -51,4 +62,37 @@ func GetMiddlewareBasicEnv(replicas int32, middlewareName, middlewareType string
 		},
 	}
 	return envs
+}
+
+func EnsureConfigMapExistsFromFile(client client.Client, ctx context.Context, namespace, configMapName, filePath, fileKey string) error {
+	// 尝试获取 ConfigMap
+	cm := &corev1.ConfigMap{}
+	err := client.Get(ctx, types.NamespacedName{Name: configMapName, Namespace: namespace}, cm)
+	if err == nil {
+		// ConfigMap 已存在
+		mlog.Infof("ConfigMap %s already exists", configMapName)
+		return nil
+	}
+
+	// 读取文件内容
+	fileContent, err := ioutil.ReadFile(filePath)
+	if err != nil {
+		return fmt.Errorf("读取文件 %s 失败: %v", filePath, err)
+	}
+
+	err = client.Create(ctx, &corev1.ConfigMap{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      configMapName,
+			Namespace: namespace,
+		},
+		Data: map[string]string{
+			fileKey: string(fileContent),
+		},
+	})
+	if err != nil {
+		mlog.Errorf("创建 ConfigMap %s 失败: %v", configMapName, err)
+		return fmt.Errorf("创建 ConfigMap %s 失败: %v", configMapName, err)
+	}
+	mlog.Infof("ConfigMap %s create success\n", configMapName)
+	return nil
 }
