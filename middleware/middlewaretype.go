f071de9fcf08d84947dbb0deaebfbcf727f1a2bd
lyl <liyinlong>
2025-04-04 20:55:40 +0800

    feat-抽象一些公共变量

--- middleware/middlewaretype.go
+++ middleware/middlewaretype.go
@@ -1,8 +1,17 @@
 package middleware
 
 const (
-	MiddlewareTypeZookeeper = "zookeeper"
-	MiddlewareTypeKafka     = "kafka"
-	MiddlewareTypeMysql     = "mysql"
-	MiddlewareTypeRedis     = "redis"
+	MiddlewareTypeZookeeper  = "zookeeper"
+	MiddlewareTypeKafka      = "kafka"
+	MiddlewareTypeMysql      = "mysql"
+	MiddlewareTypeRedis      = "redis"
+	MiddlewareTypeMinio      = "minio"
+	MiddlewareTypeMySQL      = "mysql"
+	MiddlewareTypePostgreSQL = "postgresql"
+	MiddlewareTypeRocketMQ   = "rocketmq"
+	MiddlewareTypeBookKeeper = "bookkeeper"
+	MiddlewareTypeRabbitMQ   = "rabbitmq"
+	MiddlewareTypePulsar     = "pulsar"
+	MiddlewareTypeNacos      = "nacos"
+	MiddlewareTypeApollo     = "apollo"
 )
