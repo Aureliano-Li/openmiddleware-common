d8972788165fe8cb9b0f1225d0a96536d62432c4
lyl <liyinlong>
2025-04-05 23:34:52 +0800

    feat-抽象一些公共方法

--- middleware/openmiddleware.go
+++ middleware/openmiddleware.go
@@ -1,1 +1,15 @@
 package middleware
+
+import "context"
+
+type OpenMiddleware interface {
+	PrepareInitConfigMap(ctx context.Context, middleware interface{}) error
+	PrepareMiddlewareConfigMap(ctx context.Context, middleware interface{}) error
+	PrepareStatefulSet(ctx context.Context, middleware interface{}) error
+	CreateStatefulSet(ctx context.Context, middleware interface{}) error
+	UpdateStatefulSet(ctx context.Context, middleware interface{}) error
+	PrepareService(ctx context.Context, middleware interface{}) error
+	PrepareHeadlessService(ctx context.Context, middleware interface{}) error
+	PreparePodService(ctx context.Context, middleware interface{}) error
+	UpdateMiddlewareStatus(ctx context.Context, middleware interface{}) error
+}
